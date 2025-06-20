2d01856eb4db642ebcfd1b3f2a297614c334b0e9
Harii <harikrishnamohan@proton.me>
2025-06-20 22:34:44 +0530

    images are compressed

--- public/stylesheet/convert.sh
+++ public/stylesheet/convert.sh
@@ -1,5 +1,17 @@
 #! /usr/bin/env bash
-for file in *.{png,gif,bmp,tiff,webp,jxl,jpg,jpeg}
+
+DEST=./inventory0_compressed
+mkdir -p $DEST
+
+for file in ./inventory0/*/*.{png,gif,bmp,tiff,webp,jxl,jpg,jpeg}
 do
-	[ -f "$file" ] && magick "$file" "./converted/${file%.*}.jpg"
+	if [ -f $file ]
+	then
+		STYLE_DIR=$(echo $file | cut -d'/' -f3)
+		FILE_NAME=$(echo $file | cut -d '/' -f4 | cut -d '.' -f1)
+		
+		mkdir -p "$DEST/$STYLE_DIR"
+		magick "$file" "$DEST/$STYLE_DIR/$FILE_NAME.jpg"
+	fi
+	
 done
