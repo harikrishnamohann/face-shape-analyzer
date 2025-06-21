116d7d703ae4e7c4105353b6a9ca1162b6a91910
Harii <harikrishnamohan@proton.me>
2025-06-21 19:20:40 +0530

    database is ready for use

--- server/connect.cjs
+++ server/connect.cjs
@@ -2,15 +2,15 @@ const { MongoClient } = require("mongodb");
 require("dotenv").config({ path: "./config.env" });
 const fs = require("fs");
 
-async function WriteToDb({ data }) {
+async function WriteToDb(data, collectionName) {
   const Db = process.env.ATLAS_URI;
   const client = new MongoClient(Db);
 
   try {
     await client.connect();
     const db = client.db("hair-style-analyzer");
-    const collection = db.collection("style-sheet");
-    collection.insertMany(data);
+    const collection = db.collection(collectionName);
+    await collection.insertMany(data);
   } catch (error) {
     console.error(error);
   } finally {
@@ -18,19 +18,25 @@ async function WriteToDb({ data }) {
   }
 }
 
-StyleSheet: [
- {
-  shape: ,
-  description: ,
-  hairstyles: [
-    {
-      name: ,
-      description: ,
-      images: [
+async function ReadFromDb() {
+  const Db = process.env.ATLAS_URI;
+  const client = new MongoClient(Db);
 
-      ],
-    },
-  ]
- },
+  try {
+    await client.connect();
+    const db = client.db("hair-style-analyzer");
+    const collection = db.collection("style-sheet");
+    const data = await collection.find({shape: "oval"}).toArray();
+    console.log(data[0].hairstyles[0].images[0]);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.close();
+  }
+}
 
-];
+StyleSheet = JSON.parse(fs.readFileSync("./stylesheet.json", "utf-8"))
+hairstyles = JSON.parse(fs.readFileSync("./hairstyles.json", "utf-8"));
+
+WriteToDb(StyleSheet, "style-sheet")
+WriteToDb(hairstyles, "hairstyles")
