1db8632400aaa4e9dd95842ddef9bc5b0c2a7eec
Harii <harikrishnamohan@proton.me>
2025-06-29 00:38:19 +0530

    refactoring to measurements.tsx

--- frontend/src/pages/measurementsPage.tsx
+++ frontend/src/pages/measurementsPage.tsx
@@ -28,6 +28,50 @@ export default function measurementsPage(): JSX.Element {
   ];
 
   const queryItems = ["Forehead", "Cheekbone", "Jawline", "Chin"];
+  function QueryInputElement({ item }: { item: string }): JSX.Element {
+    return (
+      <li key={item.toLowerCase()}>
+        <label htmlFor={item.toLowerCase()}>{item} </label>
+        <input
+          onInput={(event) => {
+            const target = event.target as HTMLInputElement;
+            console.log(`Input for ${item}:`, target.value);
+          }}
+          type="number"
+          id={item}
+          name={item}
+          placeholder="in cm"
+        />
+      </li>
+    );
+  }
+
+  function QueryForm(): JSX.Element {
+    return (
+      <div className="queryBox stylize">
+        <h3>your facial data</h3>
+        <ul>
+          {queryItems.map((item) => (
+            <QueryInputElement item={item} />
+          ))}
+        </ul>
+        <p className="errMsg">input error: only numerical values are allowed</p>
+      </div>
+    );
+  }
+
+  function ShowDemo({ demoName }: { demoName: string }): JSX.Element {
+    return (
+      <div className="demoContainer">
+        <img
+          className="stylize"
+          src={`/src/assets/demos/${demoName}_demo.gif`}
+          alt="forehead_demo.gif"
+        />
+        <p>{demoName} demo</p>
+      </div>
+    );
+  }
 
   return (
     <section className="stylize measurementsPage">
@@ -35,42 +79,18 @@ export default function measurementsPage(): JSX.Element {
       <div className="content">
         <h1 className="title">Let's to measure your face</h1>
         <p>
-          Grab a piece of string and a ruler (or measuring tape) to measure
-          these face dimensions in centimeters, then fill in your results below.
-          Take your time for accuracy - measuring twice is always a smart idea!
+          <b>How to measure?</b>
+          <br />
+          <b>1.</b> Grab a piece of string and a ruler or a measuring tape
+          <br />
+          <b>2.</b> Click on corresponding input fields to know how to measure
+          each part of your face.
+          <br />
+          <b>3.</b> Enter the values you got in <i>cm</i>.
         </p>
         <div className="querySection">
-          <div className="queryBox stylize">
-            <h3>your facial data</h3>
-            <ul>
-              {queryItems.map((item) => (
-                <li key={item.toLowerCase()}>
-                  <label htmlFor={item.toLowerCase()}>{item} </label>
-                  <input
-                    onInput={(event) => {
-                      const target = event.target as HTMLInputElement;
-                      console.log(`Input for ${item}:`, target.value);
-                    }}
-                    type="number"
-                    id={item}
-                    name={item}
-                    placeholder="in cm"
-                  />
-                </li>
-              ))}
-            </ul>
-            <p className="errMsg">
-              input error: only numerical values are allowed
-            </p>
-          </div>
-          <div className="demoContainer">
-            <img
-              className="stylize"
-              src="/src/assets/demos/forehead_demo.gif"
-              alt="forehead_demo.gif"
-            />
-            <p>Forehead demo</p>
-          </div>
+          <QueryForm />
+          <ShowDemo demoName="forehead" />
         </div>
       </div>
       <button className="stylize">Go!</button>
